db09f9f99eba3ddd33067ae623984221810fcd66
erblinaberisha <75163819+erblinaberisha@users.noreply.github.com>
2021-05-16 23:20:29 +0200

    Update Class1.cs

--- ConsoleApp/Client/Class1.cs
+++ ConsoleApp/Client/Class1.cs
@@ -1,20 +1,21 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Text;
 using System.Net.Sockets;
+using System.Threading;
 using System.Net;
 namespace MyClient
 {
     public class Client
     {
-        public string myIp { get; private set; }
-        public int port { get; private set; }
+        public string myIp; //{ get; private set; }
+        public int port;// { get; private set; }
         private TcpClient socketForServer;
         public bool clientStatus = true;
 
-        public NetworkStream networkStream { get; set; }
-        public StreamReader streamReader { get; set; }
-        public StreamWriter streamWriter { get; set; }
+        public NetworkStream networkStream;// { get; set; }
+        public StreamReader streamReader;// { get; set; }
+        public StreamWriter streamWriter;// { get; set; }
 
         public Client(string myIp, int port)
         {
@@ -29,10 +30,14 @@ namespace MyClient
                 {
                     socketForServer = new TcpClient(myIp.ToString(), port);
                 }
-                catch
+                catch(Exception ex)
                 {
-                    Console.WriteLine("Failed to connect to server");
-                    return;
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Failed to connect to server.");
+                    Thread.Sleep(2000);
+                    Thread.Sleep(2000);
+                    Thread.Sleep(2000);
+                    Environment.Exit(-1);
                 }
             
         }
@@ -49,7 +54,7 @@ namespace MyClient
         {
             streamWriter.Close();
             networkStream.Close();
-            streamWriter.Close();
+            streamReader.Close();
             socketForServer.Close();
         }
     }
